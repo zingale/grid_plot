5b9e759f0511e05f726ebba6d67e7bba5d3e1cbf
Michael Zingale <michael.zingale@stonybrook.edu>
2023-06-09 09:18:48 -0400

    some restructuring

--- grid_plot/__init__.py
+++ grid_plot/__init__.py
@@ -1,5 +1,24 @@
 #__all__ = ['grid_plot']
 
-from .grid_plot import (CellCentered, FDGrid, FDGrid2d, FVGrid, FVGrid2d,
-                        Grid2d, PiecewiseConstant, PiecewiseLinear,
-                        PiecewiseParabolic)
+import matplotlib as mpl
+
+# Use LaTeX for rendering
+mpl.rcParams["text.usetex"] = True
+# load the xfrac package
+mpl.rcParams["text.latex.preamble"] += r"""
+\usepackage{xfrac}
+\newcommand{\myhalf}{\sfrac{1}{2}}
+\newcommand{\mythreehalf}{\sfrac{3}{2}}
+\newcommand{\myfivehalf}{\sfrac{5}{2}}
+"""
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['mathtext.rm'] = 'serif'
+
+# font sizes
+mpl.rcParams['font.size'] = 12
+mpl.rcParams['legend.fontsize'] = 'large'
+mpl.rcParams['figure.titlesize'] = 'medium'
+
+from .grid_1d import (CellCentered, FDGrid, FVGrid, PiecewiseConstant,
+                      PiecewiseLinear, PiecewiseParabolic)
+from .grid_2d import FDGrid2d, FVGrid2d, Grid2d
