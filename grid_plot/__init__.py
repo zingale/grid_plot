21958d3ad67a988bb0b8fe11fabf68f945756ff5
Michael Zingale <michael.zingale@stonybrook.edu>
2023-06-09 09:03:24 -0400

    get the module import working

--- grid_plot/__init__.py
+++ grid_plot/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ['grid_plot']
+#__all__ = ['grid_plot']
+
+from .grid_plot import (CellCentered, FDGrid, FDGrid2d, FVGrid, FVGrid2d,
+                        Grid2d, PiecewiseConstant, PiecewiseLinear,
+                        PiecewiseParabolic)
